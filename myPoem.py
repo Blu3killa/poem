09f1e384af0e960b56da0e2c3644698ce9072e85
Blu3killa <cjbfranklin2009@gmail.com>
2024-10-07 14:18:21 +0000

    This song stinks!!

---
+++ myPoem.py
@@ -0,0 +1,9 @@
+print("""
+      (verse 1) From the screen, to the ring, to the pen to the king, wheres my crown thats my bling always drama when i ring
+      
+      (chorus) i can see it, i can feel it in my heart. smash thru the ceiling cuz im reaching for the stars 
+      
+      (Verse 2)  ohhh ohhh ohhh this is how the story goes, ohh ohh ohh this is how the story goes!
+      
+      
+      )""")
